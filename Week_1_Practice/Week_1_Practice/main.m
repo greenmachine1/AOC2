0df3fbd790048039a23557b61394ce656657538a
Cory <greenmachine@fullsail.edu>
2012-10-23 01:15:39 -0700

    added my week 1 practice folder

---
+++ Week_1_Practice/Week_1_Practice/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Week_1_Practice
+//
+//  Created by Cory Green on 10/23/12.
+//  Copyright (c) 2012 Cory Green. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
