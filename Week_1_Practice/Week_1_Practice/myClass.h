364432229cf97e9173b2be706f72c6b8a9029d4d
Cory <greenmachine@fullsail.edu>
2012-10-24 18:10:23 -0700

    Added a class to my project

---
+++ Week_1_Practice/Week_1_Practice/myClass.h
@@ -0,0 +1,13 @@
+//
+//  myClass.h
+//  Week_1_Practice
+//
+//  Created by Cory Green on 10/24/12.
+//  Copyright (c) 2012 Cory Green. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface myClass : NSObject
+
+@end
