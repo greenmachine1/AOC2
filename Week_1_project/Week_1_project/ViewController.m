caaa8b73ed211f859a08cdea07803d8efbd50ddd
Cory <greenmachine@fullsail.edu>
2012-10-25 03:39:36 -0700

    added my week 1 project initial files

---
+++ Week_1_project/Week_1_project/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Week_1_project
+//
+//  Created by Cory Green on 10/25/12.
+//  Copyright (c) 2012 Cory Green. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
