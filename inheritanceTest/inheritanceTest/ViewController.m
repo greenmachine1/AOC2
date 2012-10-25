1e04e22ec7e16ad76700e5d429eb655d5672b8c4
Cory <greenmachine@fullsail.edu>
2012-10-25 02:51:45 -0700

    working on inheritance

---
+++ inheritanceTest/inheritanceTest/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  inheritanceTest
+//
+//  Created by Cory Green on 10/25/12.
+//  Copyright (c) 2012 Cory Green. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "BaseCreature.h"
+#import "UnicornCreature.h"
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
