68fd3b8bf5892abf668ca0fac47f8f812aaf0448
Cory <greenmachine@fullsail.edu>
2012-10-26 02:24:38 -0700

    made all my labels and called on the same class outline but for 3 seperate objects

--- Week_1_project_take2/Week_1_project_take2/ViewController.m
+++ Week_1_project_take2/Week_1_project_take2/ViewController.m
@@ -26,10 +26,22 @@
     // that represent the different classes..
     GuitarFactory *newFactory = [[GuitarFactory alloc] init];
     
-    UILabel *newNewLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 300, 320, 100)];
+    UILabel *newNewLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 320, 100)];
     newNewLabel.text = [newFactory guitarChoise:JACKSON];
     newNewLabel.numberOfLines = 5;
     
+    // instantiating a new object using the same class outline!
+    UILabel *newNewLabel2 = [[UILabel alloc] initWithFrame:CGRectMake(0, 110, 320, 100)];
+    newNewLabel2.text = [newFactory guitarChoise:ESP];
+    newNewLabel2.numberOfLines = 5;
+    
+    // instantiating a new object using the same class outline!
+    UILabel *newNewLabel3 = [[UILabel alloc] initWithFrame:CGRectMake(0, 220, 320, 100)];
+    newNewLabel3.text = [newFactory guitarChoise:IBANEZ];
+    newNewLabel3.numberOfLines = 5;
+    
+    [self.view addSubview:newNewLabel3];
+    [self.view addSubview:newNewLabel2];
     [self.view addSubview:newNewLabel];
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
