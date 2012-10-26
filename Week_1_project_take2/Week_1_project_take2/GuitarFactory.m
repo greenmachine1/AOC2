c380913151c3ac8441591a2c12ce29d9fffe2bef
Cory <greenmachine@fullsail.edu>
2012-10-26 01:59:41 -0700

    working on my GuitarFactory class

---
+++ Week_1_project_take2/Week_1_project_take2/GuitarFactory.m
@@ -0,0 +1,31 @@
+//
+//  GuitarFactory.m
+//  Week_1_project_take2
+//
+//  Created by Cory Green on 10/26/12.
+//  Copyright (c) 2012 Cory Green. All rights reserved.
+//
+
+#import "GuitarFactory.h"
+
+@implementation GuitarFactory
+
+
+-(BaseGuitarClass *)guitarChoise:(int)choise
+{
+    if(choise == 0)
+    {
+            
+    }
+    else if(choise == 1)
+    {
+            
+    }
+    else if(choise == 2)
+    {
+    
+    }
+    return nil;
+}
+
+@end
